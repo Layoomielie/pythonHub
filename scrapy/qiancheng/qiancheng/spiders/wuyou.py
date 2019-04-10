1f05057f12c0fa17fffd101233907603b4e13bdc
layoomielie <layoomielie@gmail.com>
2019-04-11 00:08:52 +0800

    将parse改成parse1

--- scrapy/qiancheng/qiancheng/spiders/wuyou.py
+++ scrapy/qiancheng/qiancheng/spiders/wuyou.py
@@ -26,21 +26,21 @@ class WuyouSpider(scrapy.Spider):
 
 
     def start_requests(self):
-        # for city in self.citydict:
-        #     for cotype in self.cotypedict:
-        #         for workyear in self.workyeardict:
-        #             for companysize in self.companysizedict:
-        #                 for jobterm in self.jobtermdict:
-        #                     for degree in self.degreedict:
-        #                         dict={'city':self.citydict[city],'cotype':self.cotypedict[cotype],'workyear':self.workyeardict[workyear],'companySize':self.companysizedict[companysize],'jobTerm':self.jobtermdict[jobterm],'degree':self.degreedict[degree]}
-        #                         yield Request(self.url.format(city=city,cotype=cotype,workyear=workyear,companysize=companysize,jobterm=jobterm,degree=degree),self.parse,meta=dict)
+        for city in self.citydict:
+            for cotype in self.cotypedict:
+                for workyear in self.workyeardict:
+                    for companysize in self.companysizedict:
+                        for jobterm in self.jobtermdict:
+                            for degree in self.degreedict:
+                                dict={'city':self.citydict[city],'cotype':self.cotypedict[cotype],'workyear':self.workyeardict[workyear],'companySize':self.companysizedict[companysize],'jobTerm':self.jobtermdict[jobterm],'degree':self.degreedict[degree]}
+                                yield Request(self.url.format(city=city,cotype=cotype,workyear=workyear,companysize=companysize,jobterm=jobterm,degree=degree),self.parse1,meta=dict)
 
-        dict = {'city': 'city', 'cotype': 'cotype', 'workyear': 'workyear',
-                'companySize': 'companySize', 'jobTerm': 'jobTerm',
-                'degree': 'degree'}
-        yield Request(
-            'https://search.51job.com/list/010000,000000,0000,00,9,99,%2520,2,1.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare=',
-            self.parse1, meta=dict)
+        # dict = {'city': 'city', 'cotype': 'cotype', 'workyear': 'workyear',
+        #         'companySize': 'companySize', 'jobTerm': 'jobTerm',
+        #         'degree': 'degree'}
+        # yield Request(
+        #     'https://search.51job.com/list/010000,000000,0000,00,9,99,%2520,2,1.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare=',
+        #     self.parse1, meta=dict)
 
     def parse1(self, response):
         lines=response.css('#resultList div.el:not(div.title)')
